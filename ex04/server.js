14dbe52a5a8f911dd220ea28dcd9ebc5bd5bcd2a
juop <juliette.opdenplatz@outlook.de>
2018-04-24 17:41:20 +0200

    Add server side logging

--- ex04/server.js
+++ ex04/server.js
@@ -11,6 +11,13 @@ app.use('/users', require('./app/routes/users'));
 app.use('/hotels', require('./app/routes/hotels'));
 app.use('/bookings', require('./app/routes/bookings'));
 
+// enable logging requests
+// TODO: use some module to beautify this
+app.use((req, res, next) => {
+    console.log(req);
+    next();
+});
+
 app.listen(port, () => {
     console.log('RESTful Web Server started on *:' + port);
 });
