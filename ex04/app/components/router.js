4ce1bbd4b3fe5ea209e4091b61543bd828b7dc99
juop <juliette.opdenplatz@outlook.de>
2018-04-25 22:49:23 +0200

    Add get route

--- ex04/app/components/router.js
+++ ex04/app/components/router.js
@@ -5,23 +5,32 @@ var express = require('express');
 module.exports = (db = null) => {
     var router = express.Router();
 
+    /* GET REQUESTS */
     router.get('/', (req, res, next) => {
         res.send(JSON.stringify(db));
         next();
     });
 
+    router.get('/:id', (req, res, next) => {
+        res.send(JSON.stringify(db[req.params.id]));
+        next();
+    });
+
+    /* PUT REQUESTS */
     router.put('/', (req, res, next) => {
-        res.send('TEST PUT!');
+        res.send('TEST PUT!\n');
         next();
     });
 
+    /* POST REQUESTS */
     router.post('/', (req, res, next) => {
-        res.send('TEST POST!');
+        res.send('TEST POST!\n');
         next();
     });
 
+    /* DELETE REQUESTS */
     router.delete('/', (req, res, next) => {
-        res.send('TEST DELETE!');
+        res.send('TEST DELETE!\n');
         next();
     });
 
