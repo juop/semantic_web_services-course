0797c5594b4fd9ecd14689bad77e223ec41bb7ea
juop <juliette.opdenplatz@outlook.de>
2018-05-14 16:14:09 +0200

    Change users db

--- ex04/data/base/new_users.js
+++ ex04/data/base/new_users.js
@@ -1,28 +1,33 @@
 var w = require('./../utility/write');
 var names = require('./../names.json');
+var passwords = require('./../raw/passwords.json');
 var firstnamecount = names.firstnames.length;
 var lastnamecount = names.lastnames.length;
 
 module.exports = function(){
-	var formatted_data = {};
+	var all_users = [];
 	var id;
 	for(id=0; id<999; id++){
+		var formatted_data = {};
 		var firstnameid = Math.floor(Math.random()*firstnamecount);
 		var lastnameid = Math.floor(Math.random()*lastnamecount);
 
 		var firstname = names.firstnames[firstnameid];
 		var lastname = names.lastnames[lastnameid];
 
-		formatted_data[id] = {
+		formatted_data = {
+			id: id,
 			first_name: firstname,
 			last_name: lastname,
 			username: firstname + lastname,
 			email: firstname + lastname + "@email.net",
+			password: passwords[id*2]["password"],
 			bookings: [],
 			payments: [],
 			reviews: [],
 		}
+		all_users.push(formatted_data);
 
 	}
-	w("./users.json", JSON.stringify(formatted_data));
+	w("./users.json", JSON.stringify(all_users));
 }
