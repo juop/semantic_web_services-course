c58c4a74316835be5b2e016d5c30c2a89a85d1ab
juop <juliette.opdenplatz@outlook.de>
2018-05-14 16:32:22 +0200

    Change facilities db

--- ex04/data/base/new_facilities.js
+++ ex04/data/base/new_facilities.js
@@ -2,13 +2,16 @@ var w = require('./../utility/write');
 var ft = require('./../facility_categories.json');
 
 module.exports = function(){
-	var formatted_data = {};
+	var all_facilities = [];
 	var id;
 	for(id=0; id<999; id++){
-		formatted_data[id] = {
+		var formatted_data = {};
+		formatted_data = {
+			id: id,
 			name: ft[id%13]["name"],
 			category_id: id%13
 		}
+		all_facilities.push(formatted_data);
 	}
-	w("./facilities.json", JSON.stringify(formatted_data));
+	w("./facilities.json", JSON.stringify(all_facilities));
 }
