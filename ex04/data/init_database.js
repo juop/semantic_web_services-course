b281df8e0be36c726740227b5bfb1d84fc06744a
juop <juliette.opdenplatz@outlook.de>
2018-05-14 16:47:31 +0200

    Dumping pending changes

--- ex04/data/init_database.js
+++ ex04/data/init_database.js
@@ -8,28 +8,31 @@ fs.readdirSync(mod_path).forEach(file => {
 })
 */
 
-// FIXME: user data set relies on this so they have to be executed seperately
-names = require('./base/new_names');
-names();
 /*
 bookings = require('./base/new_bookings');
 bookings();
 hotels = require('./base/new_hotels');
 hotels();
+*/
 rooms = require('./base/new_rooms');
 rooms();
-facilities = require('./base/new_facilities');
-facilities();
+/*
 locations = require('./base/new_locations');
 locations();
 payments = require('./base/new_payments');
 payments();
 users = require('./base/new_users');
 users();
-facility_categories = require('./base/new_facility_categories');
-facility_categories();
 media = require('./base/new_media');
 media();
 reviews = require('./base/new_reviews');
 reviews();
+*/
+
+/* do not use these as we're done with them */
+/*
+facilities = require('./base/new_facilities');
+facilities();
+names = require('./base/new_names');
+names();
 */
