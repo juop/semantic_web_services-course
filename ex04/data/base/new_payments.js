1d190aa700f7a57645a4a20ffef8637e185756bc
juop <juliette.opdenplatz@outlook.de>
2018-05-14 16:40:22 +0200

    Change payments db

--- ex04/data/base/new_payments.js
+++ ex04/data/base/new_payments.js
@@ -1,16 +1,18 @@
 var w = require('./../utility/write');
 
 module.exports = function(){
-	var formatted_data = {};
+	var all_payments = [];
 	var id;
 	for(id=0; id<999; id++){
-		formatted_data[id] = {
+		var formatted_data = {};
+		formatted_data = {
+			id: id,
 			date: new Date(),
 			payment_method: "paypal",
 			amount: -1,
 			booking_id: -1,
 		}
-
+		all_payments.push(formatted_data);
 	}
-	w("./payments.json", JSON.stringify(formatted_data));
+	w("./payments.json", JSON.stringify(all_payments));
 }
