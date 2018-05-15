a73a6a6d2b3baae8ea7ac38de7eab2412a104894
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2018-05-15 15:28:02 +0200

    ex04 Refactor code in new_hotels, Create hotel DB

--- ex04/data/base/new_hotels.js
+++ ex04/data/base/new_hotels.js
@@ -1,5 +1,5 @@
-var data1 = require('./../raw/hotels_usa.json');
-// var data2 = require('./../raw/hotels_usa_2.json');
+var data1 = require('../raw/hotels_usa.json');
+var data2 = require('../raw/hotels_usa_2.json');
 var w = require('../utility/write');
 
 var locations = require('../locations.json');
@@ -28,71 +28,35 @@ module.exports = function() {
     return Array.from(set);
   }
 
+  function createHotel(name) {
+    var formatted_data = {
+      id: id,
+      name: name,
+      location: Math.floor(Math.random() * locations.length),
+      users: fillArray(0, 50, users.length),
+      rooms: fillArray(10, 50, rooms.length),
+      bookings: fillArray(0, 100, bookings.length),
+      payments: fillArray(0, 50, payments.length),
+      reviews: fillArray(0, 100, reviews.length),
+      facilities: fillArray(10, 30, facilities.length),
+      media: fillArray(0, 5, media.length)
+    }
+
+    all_hotels.push(formatted_data);
+    id++;
+  }
+
   data1["data"].forEach(function(entry) {
-    var formatted_data = {};
     var name = entry[8];
     var addr = entry[9];
 
-    if (name != addr) {
-      formatted_data = {
-        name: name,
-        id: id,
-        location: Math.floor(Math.random() * locations.length),
-        users: [],
-        facilities: [],
-        rooms: [],
-        bookings: [],
-        payments: [],
-        reviews: [],
-        media: []
-      }
+    if (name != addr)
+      createHotel(name);
+  });
 
-      formatted_data["users"] = fillArray(0, 50, users.length);
-      formatted_data["rooms"] = fillArray(10, 50, rooms.length);
-      formatted_data["bookings"] = fillArray(0, 100, bookings.length);
-      formatted_data["payments"] = fillArray(0, 50, reviews.length);
-      formatted_data["reviews"] = fillArray(0, 100, reviews.length);
-      formatted_data["facilities"] = fillArray(10, 30, facilities.length);
-      formatted_data["media"] = fillArray(0, 5, media.length);
-
-      all_hotels.push(formatted_data);
-      id++;
-    }
-  })
-
-  // data2.forEach(function(entry) {
-  //   var formatted_data = {};
-  //
-  //   formatted_data = {
-  //     name: entry.name,
-  //     id: id,
-  //     location: (id - 346),
-  //     users: [],
-  //     facilities: [],
-  //     rooms: [],
-  //     bookings: [],
-  //     payments: [],
-  //     reviews: [],
-  //     media: []
-  //   }
-  //   formatted_data["users"][0] = id;
-  //   formatted_data["users"][1] = id + 600;
-  //   formatted_data["users"][2] = id + 300;
-  //   formatted_data["payments"][0] = id;
-  //   formatted_data["payments"][1] = id + 600;
-  //   formatted_data["payments"][2] = id + 300;
-  //   formatted_data["facilities"][0] = id;
-  //   formatted_data["facilities"][1] = id - 1;
-  //   formatted_data["reviews"][0] = id;
-  //   formatted_data["rooms"][0] = id;
-  //   formatted_data["rooms"][1] = id + 1;
-  //   formatted_data["media"][0] = id % 5;
-  //   formatted_data["bookings"][0] = id;
-  //
-  //   all_hotels.push(formatted_data);
-  //   id++;
-  // })
-
+  data2.forEach(function(entry) {
+    createHotel(entry.name);
+  });
 
   w("./hotels.json", JSON.stringify(all_hotels));
 }
