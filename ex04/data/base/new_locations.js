f7967d7349557edea176f32db391217122038195
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2018-05-15 16:07:18 +0200

    ex04 Update location DB to only contain unique locations

--- ex04/data/base/new_locations.js
+++ ex04/data/base/new_locations.js
@@ -1,21 +1,25 @@
 var w = require('./../utility/write');
 var data = require('./../raw/hotels_usa_2.json');
 
-module.exports = function(){
-	var all_locations = [];
-	var id = 0;
-	data.forEach(function(entry){
-		var formatted_data = {};
-		formatted_data = {
-			id: id,
-			longitude: entry.longitude,
-			latitude: entry.latitude,
-			city: entry.city,
-			country: entry.country,
-			country_code: "USA"
-		}
-		all_locations.push(formatted_data);
-		id++
-	})
-	w("./locations.json", JSON.stringify(all_locations));
+module.exports = function() {
+  var all_locations = [];
+  var id = 0;
+  data.forEach(function(entry) {
+    var formatted_data = {
+      id: id,
+      longitude: entry.longitude,
+      latitude: entry.latitude,
+      city: entry.city,
+      country: entry.country,
+      country_code: "USA"
+    };
+
+    // Add only new locations.
+    if (id == 0 || (formatted_data.longitude !== all_locations[id - 1].longitude && formatted_data.latitude !== all_locations[id - 1].latitude)) {
+      all_locations.push(formatted_data);
+      id++;
+    }
+  });
+
+  w("./locations.json", JSON.stringify(all_locations));
 }
