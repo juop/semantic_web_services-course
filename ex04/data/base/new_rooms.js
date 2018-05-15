631d060b49e856e7b32b5ecd3e67f874838596e3
Stefan Haselwanter <Stefan.Haselwanter@student.uibk.ac.at>
2018-05-15 14:06:22 +0200

    ex04 Update facilities, facility_categories, rooms

--- ex04/data/base/new_rooms.js
+++ ex04/data/base/new_rooms.js
@@ -1,20 +1,23 @@
 var w = require('./../utility/write');
 
-module.exports = function(){
-	var all_rooms = [];
-	var id;
-	for(id=0; id<999; id++){
-		var formatted_data = {};
-		formatted_data = {
-			id: id,
-			room_no: Math.floor(id/6),
-			description: "awesome room",
-			category: "awesome",
-			size: (Math.floor(Math.random()*55) + 15).toFixed(2),
-			capacity: (Math.floor(Math.random()*6) + 1),
-			price: (Math.floor(Math.random()*200) + 20).toFixed(2),
-		}
-		all_rooms.push(formatted_data);
-	}
-	w("./rooms.json", JSON.stringify(all_rooms));
+module.exports = function() {
+  var categories = ["Single Room", "Double Room", "Triple Room", "Quadruple Room", "Quintuple Room"];
+  var all_rooms = [];
+  var id;
+  for (id = 0; id < 999; id++) {
+    var formatted_data = {};
+    var catIndex = Math.floor(Math.random() * categories.length);
+
+    formatted_data = {
+      id: id,
+      room_no: id + 1,
+      description: "awesome room",
+      category: categories[catIndex],
+      size: parseInt(Math.floor(Math.random() * 55) + 15),
+      capacity: catIndex + 1,
+      price: parseFloat((Math.random() * 200 + 20).toFixed(2)),
+    }
+    all_rooms.push(formatted_data);
+  }
+  w("./rooms.json", JSON.stringify(all_rooms));
 }
--- ex04/data/init_database.js
+++ ex04/data/init_database.js
@@ -1,31 +1,27 @@
-/*
-var fs = require('fs');
-var mod_path = './base/';
-
-fs.readdirSync(mod_path).forEach(file => {
-	var module = require(mod_path + file);
-	module();
-})
-*/
+// var fs = require('fs');
+// var mod_path = './base/';
+//
+// fs.readdirSync(mod_path).forEach(file => {
+//   var module = require(mod_path + file);
+//   module();
+// })
 
 /* do not use these as we're done with them */
-/*
-payments = require('./base/new_payments');
-payments();
-hotels = require('./base/new_hotels');
-hotels();
-locations = require('./base/new_locations');
-locations();
-bookings = require('./base/new_bookings');
-bookings();
-rooms = require('./base/new_rooms');
-rooms();
-reviews = require('./base/new_reviews');
-reviews();
-facilities = require('./base/new_facilities');
-facilities();
-users = require('./base/new_users');
-users();
-names = require('./base/new_names');
-names();
-*/
+// payments = require('./base/new_payments');
+// payments();
+// hotels = require('./base/new_hotels');
+// hotels();
+// locations = require('./base/new_locations');
+// locations();
+// bookings = require('./base/new_bookings');
+// bookings();
+// rooms = require('./base/new_rooms');
+// rooms();
+// reviews = require('./base/new_reviews');
+// reviews();
+// facilities = require('./base/new_facilities');
+// facilities();
+// users = require('./base/new_users');
+// users();
+// names = require('./base/new_names');
+// names();
